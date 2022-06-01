2e78e6fa5dfd3f297396018c8bf51cb4c9a76976
Stoian Minaiev <stoyan.minaev@gmail.com>
2022-06-01 22:57:06 +0300

    C++ Runtime build script: support Termux environment

--- build-cxx-runtime.sh
+++ build-cxx-runtime.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-# This script will build, install Chiakilisp C++ Runtime with library/header
+# This script will build, install Chiakilisp C++ Runtime with library, header
+
+# sudo is not required in Termux; its CMAKE_INSTALL_PREFIX is a bit different
+
+if [ -d /data/data/com.termux/files/ ]; then
+  PREF=/data/data/com.termux/files/usr
+else
+  PREF=/usr
+  SUDO=sudo
+fi
 
 cd runtime \
    && mkdir -p build && cd build \
-   && cmake .. -DCMAKE_INSTALL_PREFIX:PATH=/usr && make && sudo make install
+   && cmake .. -DCMAKE_INSTALL_PREFIX:PATH=$PREF && make && $SUDO make install
