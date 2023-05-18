d7350d56cf6bc56ccad7958fdd0c26ef163d0609
Carey <jedi2light@jedi2light.moe>
2023-05-19 02:56:51 +0300

    Add loop controls: break and continue, return "$control:break" and "$control:continue" respectively, work only in loops

--- chiakilisp/runtime.py
+++ chiakilisp/runtime.py
@@ -21,6 +21,8 @@ ENVIRONMENT = {
     'prn': pprint,
     'print': pprint,
     'println': pprint,  # we need more aliases for pprint!  \O/
+    'break': lambda: '$control:break',  # break control (loops)
+    'continue': lambda: '$control:continue', # continue control
     'apply': lambda fn, args: fn(*args),  # built-in apply func
     '...': Ellipsis  # make it possible to write '...' for user
 }
